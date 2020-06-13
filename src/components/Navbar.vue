82717e8214add89d1f0bd18e45d5616beeab4259
Lev Popov <levpopov1@gmail.com>
2020-06-13 19:42:14 +0100

    adds simple navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link to="/" class="navbar-brand">Navbar</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
