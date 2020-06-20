207159676eca7faab10dfaaefdf9d411edbecda1
Lev Popov <levpopov1@gmail.com>
2020-06-20 20:42:43 +0100

    moves modues into separate files

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Deck from './modules/deck'
+import Users from './modules/users'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    Deck,
+    Users
   }
 })
