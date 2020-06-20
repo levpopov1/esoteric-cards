d0252cec654149f76f13578b9c4a2d0f651a634d
Lev Popov <levpopov1@gmail.com>
2020-06-20 17:10:05 +0100

    removes helloworld component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import store from './store'
 
 Vue.config.productionTip = false
 
