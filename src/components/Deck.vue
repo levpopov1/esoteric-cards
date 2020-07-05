79dc26d6e6d82f9090f15d1945b2932007824f63
Lev Popov <levpopov1@gmail.com>
2020-07-05 05:07:30 +0100

    makes all cards in deck consistent height

--- src/components/Deck.vue
+++ src/components/Deck.vue
@@ -3,7 +3,7 @@
     <h1 class="title">Deck of Cards</h1>
     <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6">
       <div class="col mb-4" v-for="card in allCards" v-bind:key="card.id">
-        <div  class="card">
+        <div  class="card h-100">
           <div class="card-body">
             <h5 class="card-title">{{ card.title }}</h5>
           </div>
