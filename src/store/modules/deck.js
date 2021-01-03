530c5798867b2b4259b41aee6c8c2fd274a4547d
Lev Popov <levpopov1@gmail.com>
2021-01-03 03:19:07 +0000

    adds more data

--- src/store/modules/deck.js
+++ src/store/modules/deck.js
@@ -33,11 +33,77 @@ const state = {
             id: 8,
             title: "King of Clubs"
         }
+    ],
+    playingCardsCategories: [
+        {
+            id: 1,
+            title: "Theory11",
+            url: "theory11"
+        },
+        {
+            id: 2,
+            title: "USPCC",
+            url: "uspcc"
+        },
+        {
+            id: 3,
+            title: "Ellusionist",
+            url: "ellusionist"
+        },
+        {
+            id: 4,
+            title: "Art of Play",
+            url: "art-of-play"
+        },
+        {
+            id: 5,
+            title: "Bicycle",
+            url: "bicycle"
+        }, 
+        {
+            id: 6,
+            title: "Expert",
+            url: "expert"
+        }
+    ],
+    cardGamesCategories: [
+        {
+            id: 1,
+            title: "Hearhtstone",
+            url: "hearthstone"
+        },
+        {
+            id: 2,
+            title: "Magic The Gathering",
+            url: "mtg"
+        },
+        {
+            id: 3,
+            title: "Legends of Runeterra",
+            url: "lor"
+        },
+        {
+            id: 4,
+            title: "Pokemon",
+            url: "pokemon"
+        },
+        {
+            id: 5,
+            title: "Yu-Gi-Oh",
+            url: "yugioh"
+        }, 
+        {
+            id: 6,
+            title: "Gwent",
+            url: "gwent"
+        }
     ]
 };
 
 const getters = {
-    allCards: (state) => state.cards
+    allCards: (state) => state.cards,
+    playingCardsCategories: (state) => state.playingCardsCategories,
+    cardGamesCategories: (state) => state.cardGamesCategories
 };
 
 const actions = {};
