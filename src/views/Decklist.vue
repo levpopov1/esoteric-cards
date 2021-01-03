a37c39dd13951c9207499f0fd1ee42bdf004c2d8
Lev Popov <levpopov1@gmail.com>
2021-01-03 07:06:39 +0000

    adds decklist view

---
+++ src/views/Decklist.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="home">
+      <h1>Decklist</h1>
+      <Decks/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+// import Deck from '@/components/Deck'
+// import Sidebar from '@/components/Sidebar'
+import Decks from '@/components/Decks'
+
+export default {
+  name: 'Decklist',
+  components: {
+    Decks
+  }
+}
+
+</script>
