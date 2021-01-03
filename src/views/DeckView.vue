6c7bf2b048d0b47920914dcf1c96a392ddd010c9
Lev Popov <levpopov1@gmail.com>
2021-01-03 07:31:12 +0000

    adds deck view

---
+++ src/views/DeckView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Deck />
+  </div>
+</template>
+
+<script>
+  import Deck from '@/components/Deck'
+  export default {
+
+    name: 'DeckView',
+    components: {
+      Deck,
+      // Sidebar,
+    }
+  }
+</script>
+
+<style>
+
+</style>
