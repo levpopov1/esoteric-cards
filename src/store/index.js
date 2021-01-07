1ffa94f2e8220261d4e75f2067133f1d15fe6250
Lev Popov <levpopov1@gmail.com>
2021-01-07 01:10:34 +0000

    removes unused modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Deck from './modules/deck'
+import DeckAPI from './modules/deckAPI'
 import Users from './modules/users'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    Deck,
-    Users
+    // Deck,
+    Users,
+    DeckAPI
   }
 })
